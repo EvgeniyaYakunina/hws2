904bff908bb3cacd6cf45099f3f20e505d4b4a15
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-08-22 15:51:15 +0300

    Update homeworks

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Message from './message/Message'
 import MessageSender from './message-sender/MessageSender'
-import s2 from '../../s1-main/App.module.css'
+import s2 from './HW1.module.css'
 import FriendMessage from './friend-message/FriendMessage'
 import avatar from './avatar.png'
 
@@ -14,17 +14,28 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number
+    user: UserType
+    message: {
+        text: string
+        time: string
+    }
+}
+export type UserType = {
+    avatar: string
+    name: string
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'Ivan',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+        text: 'How are you, bro?', // можно менять
         time: '22:00', // можно менять
     },
 }
@@ -32,7 +43,7 @@ export const friendMessage0: MessageType = {
     id: 100,
     user: {
         avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        name: 'Alex', // можно менять
     },
     message: {
         text: 'зеркальное сообщение для тренировки css', // можно менять
@@ -43,12 +54,13 @@ export const friendMessage0: MessageType = {
 const HW1 = () => {
     return (
         <div id={'hw1'}>
+
             <div className={s2.hwTitle}>Homework #1</div>
             <div className={s2.hw}>
                 {/*проверка отображения (не менять)*/}
                 <div>
                     <Message message={message0} />
-                    <FriendMessage message={friendMessage0} />
+                    {/*<FriendMessage message={friendMessage0} />*/}
                 </div>
 
                 {/*для автоматической проверки дз (не менять)*/}
