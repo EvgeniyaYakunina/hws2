5604a881f60fdcc020f85154786174317dbfd3d7
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-10-25 16:27:47 +0300

    HW12

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -1,9 +1,10 @@
-import React, {useEffect} from 'react'
+import React, {ChangeEvent, useEffect} from 'react'
 import s from './HW12.module.css'
 import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
 import {useDispatch, useSelector} from 'react-redux'
 import {changeThemeId} from './bll/themeReducer'
+import {AppStoreType} from "../hw10/bll/store";
 
 /*
 * 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
@@ -20,10 +21,10 @@ const themes = [
 
 const HW12 = () => {
     // взять ид темы из редакса
-    const themeId = 1
-
-    const change = (id: any) => { // дописать функцию
-
+    const themeId = useSelector((state: AppStoreType )=> state.theme.themeId);
+    const dispatch = useDispatch();
+    const change = (e: ChangeEvent<HTMLSelectElement> ) => { // дописать функцию
+        dispatch(changeThemeId(+e.currentTarget.value))
     }
 
     useEffect(() => {
@@ -41,6 +42,9 @@ const HW12 = () => {
                     id={'hw12-select-theme'}
                     className={s.select}
                     // сделать переключение тем
+                    options={themes}
+                    value={themeId}
+                    onChange={change}
 
                 />
             </div>
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,23 @@
-const initState = {
+
+const initState: InitialStateType = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ActionsType): InitialStateType => { // fix any
     switch (action.type) {
         // дописать
-
+            case 'SET_THEME_ID':
+                return {...state, themeId: action.id}
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id } as const) // fix any
+
+
+export type InitialStateType ={
+    themeId: number
+}
+export type SetThemeId = ReturnType<typeof changeThemeId>
+export type ActionsType = SetThemeId
